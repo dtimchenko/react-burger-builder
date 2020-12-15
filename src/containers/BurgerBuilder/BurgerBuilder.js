b93c17e0485178869bd12cc304a6fa23f4e16dc8
Dmytro Timchenko <dmytro.timchenko@globallogic.com>
2020-12-15 19:18:09 +0200

    added withErrorHandler wrapper

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from "../../hoc/axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -69,7 +70,7 @@ class BurgerBuilder extends Component {
             }
         }
 
-        axios.post('/orders.json', order)
+        axios.post('/orders', order)
             .then(response => {
                 this.setState({ loading: false, purchasing: false });
             })
@@ -111,4 +112,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
